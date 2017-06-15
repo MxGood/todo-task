0e91fbdbb0ac03026c542d5d482f3f73f8f29a15
Maksim Gudkov <maksim_gudkov1@epam.com>
2017-06-15 12:34:40 +0400

    fixed tests

--- client/todos/__spec__/reducer-spec.ts
+++ client/todos/__spec__/reducer-spec.ts
@@ -1,116 +1,128 @@
-///// <reference path="./../../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="./../../../node_modules/@types/mocha/index.d.ts" />
 
-//import { expect } from 'chai';
+import { expect } from 'chai';
 
-//import reducer from '../reducer';
-//import { Todo } from '../model';
+import reducer from '../reducer';
+import { Todo, IState } from '../model';
 
-//import {
-//  ADD_TODO,
-//  DELETE_TODO,
-//  EDIT_TODO,
-//  COMPLETE_TODO,
-//  COMPLETE_ALL,
-//  CLEAR_COMPLETED
-//} from '../constants/ActionTypes';
+import {
+    SET_TYPEHEAD,
+    ADD_TODO,
+    DELETE_TODO,
+    EDIT_TODO,
+    COMPLETE_TODO,
+    COMPLETE_ALL,
+    CLEAR_COMPLETED
+} from '../constants/ActionTypes';
 
-//describe('todo reducer', () => {
-//  it('handles add', () => {
-//    let state: Todo[] = [{ id: 0, text: '', completed: true }];
+describe('todo reducer', () => {
 
-//    state = reducer(state, {
-//      type: ADD_TODO,
-//      payload: { text: 'hello', completed: false }
-//    });
+    it('handles add', () => {
+        let state: IState = {
+            todos: [{ id: 0, text: '', completed: true }]
+        };
 
-//    expect(state[0]).to.eql(
-//      { id: 1, text: 'hello', completed: false }
-//    );
-//  });
+        state = reducer(state, {
+            type: ADD_TODO,
+            payload: { text: 'hello', completed: false }
+        });
 
-//  it('handles delete', () => {
-//    let state: Todo[] = [{ id: 1, text: '', completed: false }];
+        expect(state[0]).to.eql(
+            { id: 1, text: 'hello', completed: false }
+        );
+    });
 
-//    state = reducer(state, {
-//      type: DELETE_TODO,
-//      payload: { id: 1 } as Todo
-//    });
+    it('handles delete', () => {
+        let state: IState = {
+            todos: [{ id: 1, text: '', completed: false }]
+        };
 
-//    expect(state).to.eql([]);
-//  });
+        state = reducer(state, {
+            type: DELETE_TODO,
+            payload: { id: 1 } as Todo
+        });
 
-//  it('handles edit', () => {
-//    let state: Todo[] = [{ id: 1, text: '', completed: false }];
+        expect(state).to.eql([]);
+    });
 
-//    state = reducer(state, {
-//      type: EDIT_TODO,
-//      payload: { id: 1, text: 'hello' } as Todo
-//    });
+    it('handles edit', () => {
+        let state: IState = {
+            todos: [{ id: 1, text: '', completed: false }]
+        };
 
-//    expect(state[0]).to.eql(
-//      { id: 1, text: 'hello', completed: false }
-//    );
-//  });
+        state = reducer(state, {
+            type: EDIT_TODO,
+            payload: { id: 1, text: 'hello' } as Todo
+        });
 
-//  it('handles complete all', () => {
+        expect(state[0]).to.eql(
+            { id: 1, text: 'hello', completed: false }
+        );
+    });
 
-//    let state: Todo[] = [
-//      { id: 1, text: '', completed: false }
-//    ];
+    it('handles complete all', () => {
 
-//    state = reducer(state, {
-//      type: COMPLETE_TODO,
-//      payload: { id: 1 } as Todo
-//    });
+        let state: IState = {
+            todos: [{ id: 1, text: '', completed: false }]
+        };
 
-//    expect(state[0]).to.eql(
-//      { id: 1, text: '', completed: true }
-//    );
-//  });
+        state = reducer(state, {
+            type: COMPLETE_TODO,
+            payload: { id: 1 } as Todo
+        });
 
-//  it('handles complete all', () => {
-//    let state: Todo[] = [
-//      { id: 1, text: '', completed: false },
-//      { id: 2, text: '', completed: true },
-//      { id: 3, text: '', completed: false }
-//    ];
+        expect(state[0]).to.eql(
+            { id: 1, text: '', completed: true }
+        );
+    });
 
-//    state = reducer(state, {
-//      type: COMPLETE_ALL,
-//      payload: {} as Todo
-//    });
+    it('handles complete all', () => {
+        let state: IState = {
+            todos: [
+                { id: 1, text: '', completed: false },
+                { id: 2, text: '', completed: true },
+                { id: 3, text: '', completed: false }
+            ]
+        };
 
-//    expect(state).to.eql([
-//      { id: 1, text: '', completed: true },
-//      { id: 2, text: '', completed: true },
-//      { id: 3, text: '', completed: true }
-//    ]);
+        state = reducer(state, {
+            type: COMPLETE_ALL,
+            payload: {} as Todo
+        });
 
-//    state = reducer(state, {
-//      type: COMPLETE_ALL,
-//      payload: {} as Todo
-//    });
+        expect(state).to.eql([
+            { id: 1, text: '', completed: true },
+            { id: 2, text: '', completed: true },
+            { id: 3, text: '', completed: true }
+        ]);
 
-//    expect(state).to.eql([
-//      { id: 1, text: '', completed: false },
-//      { id: 2, text: '', completed: false },
-//      { id: 3, text: '', completed: false }
-//    ]);
-//  });
+        state = reducer(state, {
+            type: COMPLETE_ALL,
+            payload: {} as Todo
+        });
 
-//  it('handles clear completed', () => {
-//    let state: Todo[] = [
-//      { id: 1, text: '', completed: false },
-//      { id: 2, text: '', completed: true }
-//    ];
+        expect(state).to.eql([
+            { id: 1, text: '', completed: false },
+            { id: 2, text: '', completed: false },
+            { id: 3, text: '', completed: false }
+        ]);
+    });
 
-//    state = reducer(state, {
-//      type: CLEAR_COMPLETED,
-//      payload: {} as Todo
-//    });
+    it('handles clear completed', () => {
+        let state: IState = {
+            todos: [
+                { id: 1, text: '', completed: false },
+                { id: 2, text: '', completed: true }
+            ]
+        };
 
-//    expect(state).to.eql([
-//      { id: 1, text: '', completed: false }
-//    ]);
-//  });
-//});
+        state = reducer(state, {
+            type: CLEAR_COMPLETED,
+            payload: {} as Todo
+        });
+
+        expect(state).to.eql([
+            { id: 1, text: '', completed: false }
+        ]);
+    });
+});

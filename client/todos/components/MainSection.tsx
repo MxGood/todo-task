071a4921af11f5ce5702455b77e1cc9663bca4e2
Gudkov Maksim <MGudkov@bell-main.bellintegrator.ru>
2017-07-11 17:09:04 +0400

    formated with 4 spaces

--- client/todos/components/MainSection.tsx
+++ client/todos/components/MainSection.tsx
@@ -1,89 +1,89 @@
 import * as React from 'react';
 
-import { Todo } from '../model';
+import {Todo} from '../model';
 import TodoItem from './TodoItem';
 
 interface MainSectionProps {
-  todos: Todo[];
-  typeahead: string;
-  setTypehead: (typehead: string) => any;
-  deleteTodo: (todo: Todo) => void;
-};
+    todos: Todo[];
+    typeahead: string;
+    setTypehead: (typehead: string) => any;
+    deleteTodo: (todo: Todo) => void;
+}
 
 interface MainSectionState {
-  todos: Todo[];
-};
+    todos: Todo[];
+}
 
 class MainSection extends React.Component<MainSectionProps, MainSectionState> {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: null
+        }
     }
-  }
 
-  loadData = (filter) => {
-    return new Promise((resolve) => {
-      this.timeOutId = setTimeout(() => {
-        let data = this.typeheadFilter(this.props.todos, filter);
-        resolve(data);
-      }, 500);
-    });
-  }
-
-  setData = (todos) => {
-    this.setState({ todos })
-  }
-
-  typeheadFilter = (arr, str) => {
-    if (!str) {
-      return arr;
+    loadData = (filter) => {
+        return new Promise((resolve) => {
+            this.timeOutId = setTimeout(() => {
+                let data = this.typeheadFilter(this.props.todos, filter);
+                resolve(data);
+            }, 500);
+        });
     }
-    return arr.filter(item => (item.text.indexOf(str) !== -1));
-  };
+    
+    setData = (todos) => {
+        this.setState({todos})
+    }
 
-  timeOutId: number
+    typeheadFilter = (arr, str) => {
+        if (!str) {
+            return arr;
+        }
+        return arr.filter(item => (item.text.indexOf(str) !== -1));
+    };
 
-  componentDidMount() {
-    this.loadData(this.props.typeahead).then(this.setData);
-  }
+    timeOutId: number
 
-  componentWillReceiveProps(nextProps) {
-    this.loadData(nextProps.typeahead).then(this.setData);
-  }
+    componentDidMount() {
+        this.loadData(this.props.typeahead).then(this.setData);
+    }
 
-  componentWillUnmount() {
-    clearTimeout(this.timeOutId);
-  }
+    componentWillReceiveProps(nextProps) {
+        this.loadData(nextProps.typeahead).then(this.setData);
+    }
 
-  render() {
+    componentWillUnmount() {
+        clearTimeout(this.timeOutId);
+    }
 
-    const { deleteTodo, setTypehead } = this.props;
-    const animationOfLoad = (
-      <li>
-        <div className="view">
-          <label>
-            Loading...
-          </label>
-        </div>
-      </li>);
-    const todos = this.state.todos;
+    render() {
 
-    return (
-      <section className="main">
-        <ul className="todo-list">
-          {!this.state.todos ? animationOfLoad : todos.map(todo =>
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              setTypehead={setTypehead}
-              deleteTodo={deleteTodo} />
-          )}
-        </ul>
-      </section>
-    );
-  }
+        const {deleteTodo, setTypehead} = this.props;
+        const animationOfLoad = (
+            <li>
+                <div className="view">
+                    <label>
+                        Loading...
+                    </label>
+                </div>
+            </li>);
+        const todos = this.state.todos;
+
+        return (
+            <section className="main">
+                <ul className="todo-list">
+                    {!this.state.todos ? animationOfLoad : todos.map(todo =>
+                        <TodoItem
+                            key={todo.id}
+                            todo={todo}
+                            setTypehead={setTypehead}
+                            deleteTodo={deleteTodo}/>
+                    )}
+                </ul>
+            </section>
+        );
+    }
 }
 
 export default MainSection;
